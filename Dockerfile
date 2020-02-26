280f26d27a90bbe8246979d2f252e53be3ee71c7
nico <nicolasportal92@gmail.com>
2020-02-26 23:21:35 +0100

    solved issue

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,4 @@ FROM python:3.6
 WORKDIR /
 COPY . /
 RUN pip3 install -r requirements.txt
-CMD /usr/local/bin/python ./app.py
+CMD python ./app.py
--- app.py
+++ app.py
@@ -1,16 +1,13 @@
 from flask import Flask
 from flask import request
 import subprocess
-import sys
-import os
+import shutil
 app = Flask(__name__)
 
 @app.route('/dewarp', methods=['POST'])
 def launch_app():
     file = request.files['image']
-    first_content = file.read()
-    with open('input_file', 'w+') as input_file:
-        input_file.write(str(first_content))
+    file.save('input_file')
     subprocess.run(["python", "dewarp.py", "-d", "input_file", "output.json"])
     with open("output.json", 'r') as file:
         content = file.read()
@@ -18,4 +15,4 @@ def launch_app():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
+    app.run(host="0.0.0.0", threaded = True, debug=True)
